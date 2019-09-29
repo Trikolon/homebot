ae035148cbef0d5b6e7cb57b0982b1dd313be384
Trikolon <git@pbz.im>
2019-09-29 12:40:32 +0200

    Parallel startup

--- src/index.js
+++ src/index.js
@@ -5,23 +5,19 @@ const config = require('../config.json');
 
 (async function main() {
   const hue = new PhilipsHue(config.homeAPI.hue);
-  try {
-    await hue.init();
-  } catch (error) {
-    console.error('Error while initializing PhilipsHue:', error.message);
-  }
-
   const discord = new DiscordGateway(config.messageGateway.discord);
-  try {
-    await discord.init();
-  } catch (error) {
-    console.error('Error while initializing Discord!', error.message);
-  }
 
-  const sensorSubChannel = config.messageGateway.discord.subscriptions.sensor;
-  if (sensorSubChannel) {
-    hue.on('sensor', (sensor) => {
-      discord.sendMessage(sensorSubChannel, `**Sensor update** ${sensor.name}: \`\`\`json\n${JSON.stringify(sensor.state)}\n\`\`\``);
+  Promise.all([hue.init(), discord.init()])
+    .then(() => {
+      const sensorSubChannel = config.messageGateway.discord.subscriptions.sensor;
+      if (sensorSubChannel) {
+        hue.on('sensor', (sensor) => {
+          discord.sendMessage(sensorSubChannel, `**Sensor update** ${sensor.name}: \`\`\`json\n${JSON.stringify(sensor.state)}\n\`\`\``);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error('Error while initialising!', error.message);
+      console.debug(error);
     });
-  }
 }());
